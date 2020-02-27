70ceb63a9e38ec8d95d43a8c07baeef6d1c7c263
Austin Spinazze <austin.spinazze@perficient.com>
2020-02-27 08:13:14 -0600

    Added delete feature

--- src/main/java/austinspinazze/firstspringproject/web/ProjectController.java
+++ src/main/java/austinspinazze/firstspringproject/web/ProjectController.java
@@ -16,6 +16,22 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/* @PostMapping - @PostMapping is specialized version of @RequestMapping annotation that acts as a shortcut for
+   @RequestMapping(method = RequestMethod.POST). @PostMapping annotated methods handle the HTTP POST requests matched
+   with given URI expression(@RequestMapping - the annotation is used to map web requests to Spring Controller methods)
+   ResponseEntity is a type that allows us to have more control of our JSON responses
+   @RequestBody - To put it in layman terms, the @RequestBody annotation binds the HTTPRequest body to the domain object. Spring
+   framework automatically deserializes incoming HTTPRequest to the Java object using Http Message Converters. We
+   pass the body of the request through a HttpMessageConverter to resolve the method argument depending on the content
+   type of the request. To understand this, let’s take an example of the customer registration process.
+   @Valid - Marks a property, method parameter or method return type for validation cascading. Constraints defined
+   on the object and its properties are be validated when the property, method parameter or method return type is validated.
+   In other words better error messages
+   ResponseEntity<?> (the question mark allows returning of gerneric type)
+   @PathVariable - is a Spring annotation which indicates that a method parameter should be bound to a URI template variable. If
+   the method parameter is Map<String, String> then the map is populated with all path variable names and values.
+ */
+
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
@@ -27,18 +43,6 @@ public class ProjectController {
     private MapValidationErrorService mapValidationErrorService;
 
     @PostMapping("")
-    // @PostMapping - @PostMapping is specialized version of @RequestMapping annotation that acts as a shortcut for
-    // @RequestMapping(method = RequestMethod.POST). @PostMapping annotated methods handle the HTTP POST requests matched
-    // with given URI expression(@RequestMapping - the annotation is used to map web requests to Spring Controller methods)
-    // ResponseEntity is a type that allows us to have more control of our JSON responses
-    // @RequestBody - To put it in layman terms, the @RequestBody annotation binds the HTTPRequest body to the domain object. Spring
-    // framework automatically deserializes incoming HTTPRequest to the Java object using Http Message Converters. We
-    // pass the body of the request through a HttpMessageConverter to resolve the method argument depending on the content
-    // type of the request. To understand this, let’s take an example of the customer registration process.
-    // @Valid - Marks a property, method parameter or method return type for validation cascading. Constraints defined
-    // on the object and its properties are be validated when the property, method parameter or method return type is validated.
-    // In other words better error messages
-    // ResponseEntity<?> (the question mark allows returning of gerneric type)
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
 
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
@@ -57,4 +61,10 @@ public class ProjectController {
     @GetMapping("/all")
     public Iterable<Project> getAllProjects(){return projectService.findAllProjects();}
 
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<?> deleteProject(@PathVariable String projectId) {
+        projectService.deleteProjectByIdentifier(projectId);
+
+        return new ResponseEntity<String>("Project '"+projectId+"' was deleted", HttpStatus.OK);
+    }
 }
